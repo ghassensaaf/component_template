aa9027195ec15ea45006563043b33ff8edc5185f
Restyled.io <commits@restyled.io>
2023-09-19 22:57:16 +0000

    Restyled by prettier

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import "./App.css";
 import ProductComponent from "./components/ProductComponent";
 
 function App() {
-	return(
-		<Box p={3}>
-			<Typography variant="h2">Products</Typography>
-			<Grid container spacing={2}>
-				{[1,2,3,4].map((index)=>(
-					<Grid item key={index} xs={12} md={6} lg={4} xl={3}>
-						<ProductComponent />
-					</Grid>
-				))}
-			</Grid>
-		</Box>
-	);
+  return (
+    <Box p={3}>
+      <Typography variant="h2">Products</Typography>
+      <Grid container spacing={2}>
+        {[1, 2, 3, 4].map((index) => (
+          <Grid item key={index} xs={12} md={6} lg={4} xl={3}>
+            <ProductComponent />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
 }
 
 export default App;
